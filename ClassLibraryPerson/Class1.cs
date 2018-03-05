19a6b3aa0c8c4ad5ee0f8f7242a0f9e37d895bbd
alientek <jakemog22@gmail.com>
2018-03-05 10:44:43 +0100

    Add project files.

---
+++ ClassLibraryPerson/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPerson
+{
+    public class Class1
+    {
+		public string GetName()
+		{
+			return "Jake";
+		}
+	}
+}
